56192cf22ddd7cea20074133571142b8aa115024
JoceliaHora <joceiahora@gmail.com>
2022-09-28 16:09:10 -0300

    endpoints

--- README.md
+++ README.md
@@ -1,8 +1,48 @@
-# TITULO
+
 
 ### instalando dependências
 
-#### Flask
+  
 
-https://pythonbasics.org/flask-tutorial-hello-world/
+#### Flask - primeiros passos
 
+  
+
+    https://pythonbasics.org/flask-tutorial-hello-world/
+
+  
+
+#### Flask - trabalhando com parametros
+
+  
+
+    https://www.digitalocean.com/community/tutorials/processing-incoming-request-data-in-flask-pt
+
+
+### Testando meus endpoints
+
+#### Endpoint de teste  (GET)
+
+    http://127.0.0.1:81/    
+
+#### Edpoint Cadastrar (POST)
+
+    http://127.0.0.1:81/ 
+Body
+
+    {
+	"cpf": "123.456.789.89",
+	"nome": "jocelia",
+	"endereco": "rua das ortencias"
+	}
+#### Endpoint de buscar pessoa (GET)
+
+    http://127.0.0.1:81/buscar?cpf=123
+
+#### Endpoint de listar pessoas (GET)
+
+    http://127.0.0.1:81/listar
+
+#### Endpoint de delepar pessoas (DELETE)
+
+    http://127.0.0.1:81/?cpf=123
--- src/servidor.py
+++ src/servidor.py
@@ -1,22 +1,29 @@
 #importando o flask pra criar um servidor REST
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
-def test():
-    return 'hello world'
+@app.route('/', methods=["GET"])  #define o endpoint
+def test():                       # realiza o processamento
+    return "hello world"
 
-@app.route('/cadastrar')    #path
+@app.route('/', methods=["POST"])    #path
 def cadastrar():
+    print(request.json)
     return "cadastrar"
 
-@app.route('/buscar')
+@app.route('/buscar', methods =["GET"])
 def buscar():
+    print(request.args.get('cpf'))
     return "buscar"
 
-@app.route('/listar')
+@app.route('/listar', methods = ["GET"])
 def listar():
     return "listar"
+
+@app.route('/', methods =["DELETE"])
+def deletar():
+    print(request.args.get("cpf"))
+    return "deletar"
 
 app.run(host='0.0.0.0', port=81)
